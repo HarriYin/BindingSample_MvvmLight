883ace7bbafa7b24d6c1cb0c331a30c32121cbf1
HarriYin <15655379827@163.com>
2022-06-17 15:26:33 +0800

    添加项目文件。

---
+++ MainViewModel.cs
@@ -0,0 +1,56 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BindingSample_MvvmLight
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public MainViewModel()
+        {
+
+            ShowCommand = new RelayCommand<string>(Show);
+        }
+
+        public RelayCommand<string> ShowCommand { get; set; }//申明命令
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                RaisePropertyChanged();//属性监控事件
+            }
+        }
+
+
+        private string title;
+
+        public string Title
+        {
+            get { return title; }
+            set
+            {
+                title = value;
+                RaisePropertyChanged();
+            }
+        }
+
+
+
+        private void Show(string content)
+        {
+            Name = "点击了按钮";
+            Title = "我是标题";
+            System.Windows.MessageBox.Show(content);
+        }
+
+    }
+}
